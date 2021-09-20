8ed65708002d2312b77dfc84142008e60e2c109e
Roman Böhringer <r.boehringer@opencore.ch>
2021-09-20 16:46:47 +0200

    Configured as library

--- client/hole_punching_client.cpp
+++ client/hole_punching_client.cpp
@@ -1,40 +1,19 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <netinet/in.h>
-#include <string>
-#include <iostream>
-#include <boost/asio.hpp>
-#include <boost/array.hpp>
-#include <boost/bind.hpp>
-
-#define SERVER_PORT 10000
-#define MAX_BUFFER_DATA_SIZE 65527
-
-#if __APPLE__
-    #define MSG_CONFIRM 0
-#endif
+#include "./hole_punching_client.h"
 
 using boost::asio::ip::udp;
 using boost::asio::ip::address;
 
-int main() {
-	std::string pairingName("Test");
-	std::string serverAddress("192.168.0.198");
+udp::endpoint pair(std::string pairing_name, std::string server_address) {
 	const std::size_t buffer_length = sizeof(in_addr) + sizeof(in_port_t);
 	char buffer[buffer_length];
 
 	boost::asio::io_service io_service;
     udp::socket socket(io_service);
-    udp::endpoint punching_server = udp::endpoint(address::from_string(serverAddress), SERVER_PORT);
+    udp::endpoint punching_server = udp::endpoint(address::from_string(server_address), SERVER_PORT);
     socket.open(udp::v4());
 
     boost::system::error_code err;
-    auto sent = socket.send_to(boost::asio::buffer(pairingName), punching_server, 0, err);
+    auto sent = socket.send_to(boost::asio::buffer(pairing_name), punching_server, 0, err);
 
 	int n = socket.receive_from(boost::asio::buffer(buffer), punching_server);
 	in_addr* peer_address = (in_addr*) buffer;
@@ -46,11 +25,5 @@ int main() {
 
 
 	udp::endpoint peer = udp::endpoint(address::from_string(peer_ip), peer_port_h);
-	socket.send_to(boost::asio::buffer("Ping"), peer, 0, err);
-	char recv_buffer[MAX_BUFFER_DATA_SIZE];
-	socket.receive_from(boost::asio::buffer(recv_buffer), peer);
-	std::cout << recv_buffer << std::endl;
-    
-	return 0;
+	return peer;
 }
-
---
+++ client/hole_punching_client.h
@@ -0,0 +1,12 @@
+#ifndef HOLE_PUNCHING_CLIENT_H
+#define HOLE_PUNCHING_CLIENT_H
+#include <iostream>
+#include <boost/asio.hpp>
+#include <boost/bind.hpp>
+
+#define SERVER_PORT 10000
+#define MAX_BUFFER_DATA_SIZE 65527
+
+boost::asio::ip::udp::endpoint pair(std::string pairing_name, std::string server_address);
+
+#endif
