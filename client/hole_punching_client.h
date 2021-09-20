f155c3fd80b0534ff47898f5d852ac34a90df7e0
Roman Böhringer <r.boehringer@opencore.ch>
2021-09-20 21:32:51 +0200

    changed max buffer size for AWS

--- client/hole_punching_client.h
+++ client/hole_punching_client.h
@@ -5,7 +5,7 @@
 #include <boost/bind.hpp>
 
 #define SERVER_PORT 10000
-#define MAX_BUFFER_DATA_SIZE 65527
+#define MAX_BUFFER_DATA_SIZE 65500
 
 boost::asio::ip::udp::endpoint pair(boost::asio::ip::udp::socket& socket, std::string pairing_name, std::string server_address);
 
